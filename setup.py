19503e47cf4993f248c8b0f0ed5514a02157990e
shaunsephton <shaunsephton@gmail.com>
2010-06-17 17:07:06 +0200

    polish

--- setup.py
+++ setup.py
@@ -2,11 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-gizmo',
-    version='dev',
+    version='0.0.1',
     description='Django app allowing for configurable targetting of template inclusion tags.',
-    author='Praekelt Consulting',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-gizmo',
+    license='BSD',
+    url='http://github.com/praekelt/panya-gallery',
     packages = find_packages(),
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
 )
