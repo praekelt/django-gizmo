892758ebb87d51a387ed3d8f0041c52526256686
shaunsephton <shaun@praekeltconsulting.com>
2011-01-06 13:49:30 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-gizmo',
-    version='0.0.1',
+    version='0.0.2',
     description='Django app allowing for configurable targetting of template inclusion tags.',
     long_description = open('README.rst', 'r').read(),
     author='Praekelt Foundation',
